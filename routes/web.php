2695a6c3af989c10d1b53c2a959db98f25b503ef
Lindsay <lindsay@webandwolf.com>
2025-09-04 00:15:42 -0700

    product detail template and routing

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsletterController;
 
+// Route to individual product/wine pages; template: resources/views/product.antlers.html
+Route::statamic('/product/{handle}', 'product');
+
 // Route for handling newsletter subscriptions.
 Route::post('/newsletter', NewsletterController::class)->name('newsletter');
 
